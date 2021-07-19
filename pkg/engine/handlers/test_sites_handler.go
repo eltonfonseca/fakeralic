030cbc3a627f7aab3aede40cca7c6450ecfc6db9
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2021-07-18 21:36:36 -0300

    added test request site

--- pkg/engine/handlers/test_sites_handler.go
+++ pkg/engine/handlers/test_sites_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fakeralic/pkg/engine/interfaces"
 	"fakeralic/pkg/engine/models"
+	"net/http"
 
 	"github.com/sirupsen/logrus"
 )
@@ -10,7 +11,22 @@ import (
 type TestSitesHandler struct{}
 
 func (ts *TestSitesHandler) Execute(m *models.Message) (interfaces.Handler, error) {
-	logrus.Info("... Testing sites and call next handler WriteLogsHandler ...")
+	for index, host := range m.Hosts {
+		response, err := http.Get(host.Name)
+
+		if err != nil {
+			logrus.WithError(err).Error("error on make request")
+			break
+		}
+
+		if response.StatusCode == 200 {
+			m.Hosts[index].Online = true
+		} else {
+			m.Hosts[index].Online = false
+		}
+
+		m.Hosts[index].StatusCode = response.StatusCode
+	}
 
 	return &WriteLogsHandler{}, nil
 }
